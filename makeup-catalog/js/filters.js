c5882c3b9a725e939f9f56b63df5a06a8b64ffd1
Marcus Rocha <32283039+marcusvrsousa@users.noreply.github.com>
2023-02-23 13:19:33 -0300

    alteração na criação da lista de produtos

--- makeup-catalog/js/filters.js
+++ makeup-catalog/js/filters.js
@@ -1,4 +1,5 @@
 import { Service } from "./service.js";
+import createProductList from "./productsList.js";
 
 const filterBrand = () => {
     const valueSelect = document.getElementById('brand');
@@ -12,33 +13,12 @@ const filterBrand = () => {
 
         //chamando o serviço que filtra marcas
         Service.filterBrand(brand)
-            .then((response) =>  {
-                const products = response.data;
-                
-                products.forEach(product => {
-                    const product_list = document.querySelector('.product_list');
-                    const element = document.createElement('li');
-                    element.className = 'product_list-item';
-                    if(product.price != '0.0'){
-    
-                        element.innerHTML = `
-                            <figure class="product_image">
-                                <img src="${product.api_featured_image}" width="215" height="215">
-                            </figure>
-                            <section class="product_description">
-                                <h2 class="product_name"> ${product.name}</h2>
-                                <span class="product_brand"> ${product.brand}</span>
-                                <span class="product_price">$ ${product.price}</span>   
-                            </section>
-                        `
-                        //atribuindo ao elemento pai um elemento filho
-                        product_list.appendChild(element);    
-                    }
-                });
-            })
+        .then((response) =>  {
+            //criando no DOM a lista de produtos
+            createProductList(response);
+        })
     }
 }
-
 
 filterBrand();
 
--- makeup-catalog/js/productsList.js
+++ makeup-catalog/js/productsList.js
@@ -1,35 +1,39 @@
 import {Service} from './service.js'; 
 
+const createProductList = (response) =>{
+    //capturando os objetos obtidos
+    const products = response.data;
+
+    //Para cada produto encontrado cria-se uma lista com imagem, nome, marca e preço
+    products.forEach(product => {
+        const product_list = document.querySelector('.product_list');
+
+        const element = document.createElement('li');
+        element.className = 'product_list-item';
+        if(product.price != '0.0'){
+
+            element.innerHTML = `
+                <figure class="product_image">
+                    <img src="${product.api_featured_image}" width="215" height="215">
+                </figure>
+                <section class="product_description">
+                    <h2 class="product_name"> ${product.name}</h2>
+                    <div class="brand_price">
+                        <span class="product_brand"> ${product.brand}</span>
+                        <span class="product_price">$ ${product.price}</span>  
+                    </div> 
+                </section>
+            `
+            //atribuindo ao elemento pai um elemento filho
+            product_list.appendChild(element);
+        }
+    });
+}
+
 function productsList(){
 Service.request()
-.then((response) => {
-        //capturando os objetos obtidos
-        const products = response.data;
-
-        //Para cada produto encontrado cria-se uma lista com imagem, nome, marca e preço
-        products.forEach(product => {
-            const product_list = document.querySelector('.product_list');
-    
-            const element = document.createElement('li');
-            element.className = 'product_list-item';
-            if(product.price != '0.0'){
-
-                element.innerHTML = `
-                    <figure class="product_image">
-                        <img src="${product.api_featured_image}" width="215" height="215">
-                    </figure>
-                    <section class="product_description">
-                        <h2 class="product_name"> ${product.name}</h2>
-                        <span class="product_brand"> ${product.brand}</span>
-                        <span class="product_price">$ ${product.price}</span>   
-                    </section>
-                `
-                //atribuindo ao elemento pai um elemento filho
-                product_list.appendChild(element);
-            }
-        });
-
-        
+    .then((response) => {
+        createProductList(response); 
     })
     .catch(function (error) {
         // handle error
@@ -40,4 +44,7 @@ Service.request()
     });
 };
 
-//productsList();
+productsList();
+
+
+export default createProductList
